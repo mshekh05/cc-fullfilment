922d991ee3f8ca45c5c73f4094c3ec2f07162b26
mshekh05 <mohdshekh05@gmail.com>
2018-04-22 20:50:57 -0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -13,52 +13,75 @@ router.get('/', (req, res) => {
 
 function getMinimumFlight(flights) {
   var minFlight = flights[0];
-  console.log(flights.length)
-  for (var flight in flights) {
+  var flight = 1;
+  while (flight <= 5) {
     var curMin = minFlight.fare.grossamount
     if (curMin > flights[flight].fare.grossamount) {
       minFlight = flights[flight]
     }
+    flight += 1;
   }
   return minFlight
 }
 
 router.post('/dialog', (request, response) => {
   try {
-    
-  
-  var intent = request.body.queryResult.intent.displayName
-  if (intent === "option1-flightsearch - more - more"){
-    var destination =  request.body.queryResult.parameters.geodestination
-    var source =  request.body.queryResult.outputContexts[0].parameters.geosource
-  return response.json({
-    "fulfillmentText": destination +"        "+ source
-  });
 
-  }
-  else if (intent === "Default Welcome Intent"){
-    var speech =
-    request.body.queryResult &&
-    request.body.queryResult.parameters &&
-    request.body.queryResult.parameters.echoText
-      ? request.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again.";
+
+    var intent = request.body.queryResult.intent.displayName
+    if (intent === "option1-flightsearch - more - more") {
+      
+      var destination = request.body.queryResult.parameters.geodestination
+      var source = request.body.queryResult.outputContexts[0].parameters.geosource
+      var sourceIata = airports.findWhere({ city: source }).get('iata')
+      var destinationIata = airports.findWhere({ city: destination }).get('iata')
+
+      if (sourceIata == '' || destinationIata == '') {
+        return response.json({
+          "fulfillmentText": "Seems like some problem. Speak again."
+        });
+      }
+
+      const url = 'https://developer.goibibo.com/api/search/?app_id=738f476c&app_key=f680503962623e838c52be41f0094b69&source=' + sourceIata + '&destination=' + destinationIata + '&dateofdeparture=20180719&seatingclass=E&adults=1&children=0&infants=0&counter=100'
+      axios.get(url)
+        .then(res => {
+          // res.send(getMinimumFlight(res.data.data.onwardflights))
+          minFlightCost = response.data.data.onwardflights[0].fare.grossamount
+          return response.json({
+            "fulfillmentText": destination + "        " + source + "     " + minFlightCost
+          });
+        })
+        .catch(error => {
+          console.log(error);
+          return response.json({
+            "fulfillmentText": "Seems like some problem. Speak again."
+          });
+        });
+    }
+    else if (intent === "Default Welcome Intent") {
+      var speech =
+        request.body.queryResult &&
+          request.body.queryResult.parameters &&
+          request.body.queryResult.parameters.echoText
+          ? request.body.queryResult.parameters.echoText
+          : "Seems like some problem. Speak again.";
+      return response.json({
+        "fulfillmentText": speech
+      });
+    }
+
+  } catch (error) {
+    console.log(request.body.queryResult)
+    console.log(error)
     return response.json({
-    "fulfillmentText": speech
-  });
+      "fulfillmentText": "Seems like some problem. Speak again."
+    });
+
+
+
+
   }
 
-} catch (error) {
-  console.log(request.body.queryResult)
-  console.log(error)
-  return response.json({
-    "fulfillmentText": "Seems like some problem. Speak again."
-  });
-  
-
-
-    
-}
 
 
 
@@ -67,10 +90,9 @@ router.post('/dialog', (request, response) => {
 
 
 
-  
- 
 
-  
+
+
 });
 
 
