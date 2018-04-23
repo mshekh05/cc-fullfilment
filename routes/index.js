80360ab60ba7795b24b13fd047c391f4b1b9d767
mshekh05 <mohdshekh05@gmail.com>
2018-04-22 20:04:29 -0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.post('/dialog', (request, response) => {
       ? request.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return response.json({
-    "fulfillmentText": "This is a text response"
+    "fulfillmentText": speech
   });
 });
 
